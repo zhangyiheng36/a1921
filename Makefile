9a7b6413955da8ff61151824a56509830f82cf52
root <root@LAPTOP-SQPEUQSR.localdomain>
2020-11-26 20:50:28 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+#thsi is sample
+testCal:paixu.h paixu.c testpaixu.c
+	gcc paixu.h paixu.c testpaixu.c -o testpaixu
+clean:
+	rm testCal
---
+++ paixu.h
@@ -0,0 +1,14 @@
+#ifndef paixu_h
+#define paixu_h
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+void Bubble1(int a[],int n);
+void InsertSort(int a[],int n);
+void SelectSort(int a[],int n);
+void QSort(int a[],int low,int high);
+void QuickSort(int a[],int n);
+
+#endif
